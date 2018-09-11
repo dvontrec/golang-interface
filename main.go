cbac51485b0cb1370f92a95f3b8387128bae4b78
Dvontre Coleman <dvontrec@gmail.com>
2018-09-11 05:05:24 -0400

    convert response to bytes and print as string

--- main.go
+++ main.go
@@ -7,11 +7,19 @@ import (
 )
 
 func main() {
+	// saves the response and error of a get request to google.com
 	resp, err := http.Get("http://google.com")
 	if err != nil {
 		fmt.Println("Error: ", err)
 		os.Exit(1)
 	}
 
-	fmt.Println(resp)
+	// create a byte slice with lots of empty spaces, read function does not resize slice, only adds until full
+	bs := make([]byte, 99999)
+
+	// Saves the response body to the byte slice
+	resp.Body.Read(bs)
+
+	// prints out the body as a string
+	fmt.Println(string(bs))
 }
