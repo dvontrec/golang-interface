323d20067150acfd8dea0f0ecbaf86b9f4d32de0
Dvontre Coleman <dvontrec@gmail.com>
2018-09-11 05:38:49 -0400

    Refactor with custom writer

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"os"
 )
 
+type logWriter struct{}
+
 func main() {
 	// saves the response and error of a get request to google.com
 	resp, err := http.Get("http://google.com")
@@ -15,6 +17,18 @@ func main() {
 		os.Exit(1)
 	}
 
+	// Creates a new logWriter
+	lw := logWriter{}
+
 	// Takes the response body and writes it to the os.Stdout
-	io.Copy(os.Stdout, resp.Body)
+	io.Copy(lw, resp.Body)
+
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	// prints the byte slice as a string
+	fmt.Println(string(bs))
+	fmt.Println("just wrote this many bytes: ", len(bs))
+	// returns the number of bytes and passes no error
+	return len(bs), nil
 }
